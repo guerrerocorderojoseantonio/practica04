32790e4c5455354a6e2ecc376fa94bd245366f5f
unknown <jaguerrero@iespuntadelverde.es>
2020-11-08 23:31:34 +0100

    2 commit

--- src/consultas.js
+++ src/consultas.js
@@ -1,2 +1,6 @@
 //Los corredores de Yamaha que sean italianos
-db.motogp.find({nacionalidad="ITA"})
+db.motogp.find({"equipo.marca":"Yamaha", nacionalidad:"ITA"})
+//Los corredores de Yamaha que sean italianos sin diferenciar mayúsculas de minúsculas
+db.motogp.find({"equipo.marca":"Yamaha", nacionalidad: { $regex: /ITA/i}})
+//Corredores que empiecen por una V en su nombre y sean españoles
+db.motogp.find({nacionalidad:"ESP", nombre: { $regex: /^v/, $options: 'i' }})
