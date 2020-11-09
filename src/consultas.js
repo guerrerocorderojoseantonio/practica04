4c0618c1b85925541e279dddcf057a6fef21c617
unknown <jaguerrero@iespuntadelverde.es>
2020-11-09 18:47:17 +0100

    3 commit

--- src/consultas.js
+++ src/consultas.js
@@ -4,3 +4,15 @@ db.motogp.find({"equipo.marca":"Yamaha", nacionalidad:"ITA"})
 db.motogp.find({"equipo.marca":"Yamaha", nacionalidad: { $regex: /ITA/i}})
 //Corredores que empiecen por una V en su nombre y sean españoles
 db.motogp.find({nacionalidad:"ESP", nombre: { $regex: /^v/, $options: 'i' }})
+//Italianos y franceses
+db.motogp.find({ $or: [ { nacionalidad:"FRA" }, {nacionalidad: "ITA" }]})
+//Mas puntos de 50 y menos 100
+db.motogp.find({ $and: [{puntos: {$lt:100}, puntos:{$gt:50}}]})
+//Equipo honda
+db.motogp.find({"equipo.marca":{$eq:"Honda"}})
+//Patrocinio Repsol y RedBull
+db.motogp.find({"equipo.patrocinio": { $in: ["Repsol", "RedBull"]}})
+//Numero diferente a 9
+db.motogp.find({"equipo.numero": { $ne: 9}})
+//Numeros diferentes a 1, 2 y 6
+db.motogp.find({"equipo.numero": { $nin: [ 1,2, 9 ] } } )
